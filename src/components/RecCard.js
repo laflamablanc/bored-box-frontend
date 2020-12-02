3bdbc49f2195b9548595465657de7b3b602f0275
Sean LaFlam <sean.laflam@gmail.com>
2020-12-01 23:45:53 -0500

    Nested Routing from Recs Page Working

--- src/components/RecCard.js
+++ src/components/RecCard.js
@@ -1,43 +1,44 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import Grid from '@material-ui/core/Grid';
-import {addGameToBox, addGameCollection} from '../redux/actions'
+import {addGameToBox} from '../redux/actions'
+import { useHistory } from "react-router-dom";
 
 
-class BoxGameCard extends React.Component {
+const RecCard = (props) => {
 
-  localClickHandler = (e) => {
-    const game = this.props.game
-    const currentBox = this.props.currentBox
-    this.props.addGameToBox(game, currentBox)
+  const localClickHandler = (e) => {
+    const game = props.game
+    const currentBox = props.currentBox
+
+    props.addGameToBox(game, currentBox)
   }
 
-  render(){
+    let history = useHistory()
     return(
       <div className="box-game-card">
         <React.Fragment>
           <Grid item xs ={3} spacing ={1}>
-            <img src ={this.props.game.image} alt = {this.props.game.title}/>
+            <img src ={props.game.image} alt = {props.game.title} onClick = {()=>history.push("/games/" + props.game.id)}/>
           </Grid>
           <Grid xs ={3}>
-            <h2> {this.props.game.name}</h2>
+            <h2> {props.game.name}</h2>
           </Grid>
           <Grid xs ={3}>
             <h3> BoredScore™️:
               <div>
-                {this.props.score}
+                {props.score}
               </div>
             </h3>
           </Grid>
           <Grid xs ={3}>
-            <h3> Avg. Rating: {this.props.game.rating.toFixed(2)}</h3>
-            <button name="add-rec" onClick = {this.localClickHandler}> Add to Box </button>
+            <h3> Avg. Rating: {props.game.rating.toFixed(2)}</h3>
+            <button name="add-rec" onClick = {localClickHandler}> Add to Box </button>
           </Grid>
         </React.Fragment>
       </div>
     )
   }
-}
 
 const mapDispatchToProps = (dispatch) => {
   return {addGameToBox: (game, currentBox) => dispatch(addGameToBox(game, currentBox))}
@@ -47,4 +48,4 @@ const mapStateToProps = (state) => {
   return {currentBox: state.currentBox}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(BoxGameCard)
+export default connect(mapStateToProps, mapDispatchToProps)(RecCard)
