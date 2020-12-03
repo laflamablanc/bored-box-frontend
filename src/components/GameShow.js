f0e1b42785ea39ccf8f0dd55f900f0d6fb671010
Sean LaFlam <sean.laflam@gmail.com>
2020-12-03 15:27:33 -0500

    Show Page Stylinng

--- src/components/GameShow.js
+++ src/components/GameShow.js
@@ -3,12 +3,22 @@ import Graph from '../components/Graph';
 
 const GameShow = props => {
   return (
-    <div>
-      <h1>{props.game.name}</h1>
-      <img src ={props.game.image} alt={props.game.name}/>
-      <Graph game= {props.game} userProfile ={props.userProfile}/>
-      <h4>Description: </h4>
-      <p> {props.game.description} </p>
+    <div className = "game-show-container">
+      <div className ="game-show-section">
+        <h1>{props.game.name}</h1>
+        <img src ={props.game.image} alt={props.game.name}/>
+        <Graph game= {props.game} userProfile ={props.userProfile}/>
+      </div>
+      <div className ="game-description-section">
+        <h4>Genre: </h4>
+        <p> {props.game.genre} </p>
+        <h4>Description: </h4>
+        <p> {props.game.description} </p>
+        <h4>Price: </h4>
+        <p> ${props.game.price} </p>
+        <h4>Number of Players: </h4>
+        <p> {props.game.min_players} - {props.game.max_players} </p>
+      </div>
     </div>
   );
 }
