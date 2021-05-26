a5aebdfb9f71ecae4353d576424149cc7b897ce9
Sean LaFlam <sean.laflam@gmail.com>
2021-05-25 23:13:00 -0400

    Cleaned up unused code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,9 @@
 import withRoot from '../withRoot';
 // --- Post bootstrap -----
 import React from 'react';
-// import ProductCategories from './modules/views/ProductCategories';
-// import ProductSmokingHero from './modules/views/ProductSmokingHero';
-// import AppFooter from './modules/views/AppFooter';
 import HeroImage from '../components/HeroImage';
 import HowItWorks from '../components/HowItWorks';
 import Graph from '../components/Graph';
-// import ProductValues from './modules/views/ProductValues';
-// import ProductHowItWorks from './modules/views/ProductHowItWorks';
-// import ProductCTA from './modules/views/ProductCTA';
-// import AppAppBar from './modules/views/AppAppBar';
 
 function Home() {
   return (
